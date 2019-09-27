7e26158f9a3930a487b31605037f1fb0b5915fcc
kohou <hoh_ko@ricoh-japan.co.jp>
2019-09-27 14:16:06 +0900

    add example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,6 @@
 
 * improve heath point
 
-## [0.1.6] - 2019/09/27
+## [0.1.7] - 2019/09/27
 
-* add test demo
+* add example
--- example/example.dart
+++ example/example.dart
@@ -1,15 +1,27 @@
 import 'package:flutter_github_api/flutter_github_api.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 class Example {
-  void main() {
-    test('list issues by repository', () {
-      final github = createGitHubClient();
-      var slug = RepositorySlug('houko', 'flutter_github_api');
-      Stream<Issue> issueStream = github.issues.listByRepo(slug);
-      issueStream.listen((data) {
-        expect(data, data != null);
-      });
+  static final github = createGitHubClient();
+  static final slug = RepositorySlug('houko', 'flutter_github_api');
+
+  static Stream<Issue> listIssuesByRepository() {
+    Stream<Issue> issueStream = github.issues.listByRepo(slug);
+    issueStream.listen((data) {
+      print(data);
+      // do something
     });
+    return issueStream;
+  }
+
+  static Stream<Repository> listRepository() {
+    Stream<Repository> repositoryStream =
+        github.repositories.listRepositories(type: 'houko');
+
+    repositoryStream.listen((data) {
+      print(data);
+      // do something
+    });
+
+    return repositoryStream;
   }
 }
